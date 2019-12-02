7a496f8909298cf0f5c3e222763a4ee512c9e2db
Simon Bauer <bauer-s-public@outlook.com>
2019-12-02 18:27:01 +0100

    Removed Proxy by default

--- lib/api/apiClient.dart
+++ lib/api/apiClient.dart
@@ -26,9 +26,9 @@ class ApiClient {
 
     (client.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
         (client) {
-      client.findProxy = (url) {
-        return "PROXY 192.168.11.10:8888";
-      };
+      //client.findProxy = (url) {
+      //  return "PROXY 192.168.11.10:8888";
+      //};
 
       client.badCertificateCallback = (cert, host, port) => Platform.isAndroid;
     };

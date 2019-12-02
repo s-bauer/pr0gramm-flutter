24991c09336f18d114bb25a0d336db8487a44cd2
Simon Bauer <bauer-s-public@outlook.com>
2019-12-02 19:04:36 +0100

    Added Tags

--- lib/views/widgets/postPage.dart
+++ lib/views/widgets/postPage.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:pr0gramm/entities/linkedComments.dart';
 import 'package:pr0gramm/entities/postInfo.dart';
@@ -15,8 +14,6 @@ class PostPage extends StatefulWidget {
   _PostPageState createState() => _PostPageState();
 }
 
-
-
 class _PostPageState extends State<PostPage> {
   final ItemProvider _itemProvider = ItemProvider();
   PageController _controller;
@@ -24,10 +21,43 @@ class _PostPageState extends State<PostPage> {
   List<LinkedComment> linkComments(PostInfo postInfo) {
     final plainComments = postInfo.info.comments;
     return plainComments
-      .where((c) => c.parent == 0)
-      .map((c) => LinkedComment.root(c, plainComments))
-      .toList()
-      ..sort((a, b) => b.comment.confidence.compareTo(a.comment.confidence));
+        .where((c) => c.parent == 0)
+        .map((c) => LinkedComment.root(c, plainComments))
+        .toList()
+          ..sort(
+              (a, b) => b.comment.confidence.compareTo(a.comment.confidence));
+  }
+
+  Widget buildTags(BuildContext context, PostInfo info) {
+    var tags = info.info.tags
+      ..sort((a, b) => b.confidence.compareTo(a.confidence));
+
+    var tagWidgets = tags.map((t) {
+      return Padding(
+        padding: const EdgeInsets.only(right: 5),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            border: Border.all(color: Colors.grey),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
+            child: Text(
+              t.tag,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      );
+    }).toList();
+
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(children: tagWidgets),
+      ),
+    );
   }
 
   @override
@@ -45,14 +75,16 @@ class _PostPageState extends State<PostPage> {
           return FutureBuilder<PostInfo>(
             future: _itemProvider.getItemWithInfo(index),
             builder: (context, snapshot) {
-              if(!snapshot.hasData)
+              if (!snapshot.hasData)
                 return Center(child: CircularProgressIndicator());
 
               var comments = linkComments(snapshot.data);
+
               return SingleChildScrollView(
                 child: Column(
                   children: <Widget>[
                     PostView(item: snapshot.data.item),
+                    buildTags(context, snapshot.data),
                     Column(children: comments.map((c) => c.build()).toList())
                   ],
                 ),

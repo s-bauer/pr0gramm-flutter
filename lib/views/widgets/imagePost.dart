1cab72ceba432709bf7a2c972ca0190561dc65ab
Mathis Michel <mathis.michel@outlook.de>
2019-12-04 01:24:57 +0100

    implement thumb loading for video and images

--- lib/views/widgets/imagePost.dart
+++ lib/views/widgets/imagePost.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:pr0gramm/api/dtos/getItemsResponse.dart';
 import 'package:pr0gramm/services/imageProvider.dart' as imgProv;
 
-
 class ImagePost extends StatefulWidget {
   final Item item;
 
@@ -17,14 +16,26 @@ class _ImagePostState extends State<ImagePost> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _imageProvider.getImage(widget.item),
-      builder: (context, snap) {
-        if(snap.hasData)
-          return Image.memory(snap.data);
-
-        return Container();
-      },
-    );
+    var width = MediaQuery.of(context).size.width;
+    var height =
+        (width / (widget.item.width + 0.0)) * (widget.item.height + 0.0);
+    return Container(
+        width: width,
+        height: height,
+        child: FutureBuilder(
+          future: _imageProvider.getImage(widget.item),
+          builder: (context, snap) {
+            if (snap.hasData) return Image.memory(snap.data);
+            return FutureBuilder(
+              future: _imageProvider.getThumb(widget.item),
+              builder: (context, snap) {
+                if (snap.hasData)
+                  return Image.memory(snap.data,
+                      width: width, height: height, fit: BoxFit.fill);
+                return Container(color: Colors.white);
+              },
+            );
+          },
+        ));
   }
 }
--- lib/views/widgets/videoPost.dart
+++ lib/views/widgets/videoPost.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pr0gramm/api/dtos/getItemsResponse.dart';
 import 'package:video_player/video_player.dart';
+import 'package:pr0gramm/services/imageProvider.dart' as imgProv;
 
 class VideoPost extends StatefulWidget {
   final Item item;
@@ -13,6 +14,7 @@ class VideoPost extends StatefulWidget {
 
 class _VideoPostState extends State<VideoPost> {
   VideoPlayerController _controller;
+  final imgProv.ImageProvider _imageProvider = imgProv.ImageProvider();
 
   @override
   void initState() {
@@ -30,6 +32,9 @@ class _VideoPostState extends State<VideoPost> {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height =
+        (width / (widget.item.width + 0.0)) * (widget.item.height + 0.0);
     return Stack(
       children: <Widget>[
         GestureDetector(
@@ -41,10 +46,25 @@ class _VideoPostState extends State<VideoPost> {
           },
           child: _controller?.value?.initialized ?? false
               ? AspectRatio(
-            aspectRatio: _controller.value.aspectRatio,
-            child: VideoPlayer(_controller),
-          )
-              : CircularProgressIndicator(),
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                )
+              : Container(
+                  color: Colors.white,
+                  width: width,
+                  height: height,
+                  child: FutureBuilder(
+                    future: _imageProvider.getThumb(widget.item),
+                    builder: (context, snap) {
+                      if (snap.hasData)
+                        return Image.memory(snap.data,
+                            width: width,
+                            height: height,
+                            fit: BoxFit.fill);
+
+                      return Container(color: Colors.white);
+                    },
+                  )),
         ),
       ],
     );

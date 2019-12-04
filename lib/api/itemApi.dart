8783629a820893c10acd9363e1cbc46d8bd78be7
Mathis Michel <mathis.michel@outlook.de>
2019-12-04 20:08:24 +0100

    add new endpoint get item by id

--- lib/api/itemApi.dart
+++ lib/api/itemApi.dart
@@ -8,6 +8,22 @@ class ItemApi extends BaseApi {
     final response = await client.get("/items/get?flags=$flags${promoted ? "&promoted=1" : ""}${older != null ? "&older=$older" : ""}");
     return GetItemsResponse.fromJson(response.data);
   }
+
+  Future<GetItemsResponse> getItemsById(int id,
+      {int flags, bool promoted = false}) async {
+    var response;
+    var temp;
+
+    print("/items/get?id=$id&flags=$flags${promoted ? "&promoted=1" : ""}");
+    try {
+      response = await client.get(
+          "/items/get?id=$id&flags=$flags${promoted ? "&promoted=1" : ""}");
+      temp = GetItemsResponse.fromJson(response?.data);
+    } catch (e) {
+    }
+    return temp;
+  }
+
   Future<ItemInfoResponse> getItemInfo(int itemId) async {
     final response = await client.get("/items/info?itemId=$itemId");
     return ItemInfoResponse.fromJson(response.data);

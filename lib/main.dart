6bdb951a7c89978d40664b167743a409782bc28d
Mathis Michel <mathis.michel@outlook.de>
2019-12-04 20:13:33 +0100

    Create new service to handle links 'AppLinkHandler' make main Stateful (maybe it could be reverted now) use AppLinkHandler after Navigation is available add first link handler for Top links

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,40 @@
 import "package:flutter/material.dart";
+import 'package:pr0gramm/services/AppLinkHandler.dart';
+import 'package:pr0gramm/services/itemProvider.dart';
 import 'package:pr0gramm/views/homeView.dart';
+import 'package:pr0gramm/views/widgets/postPage.dart';
 import 'package:pr0gramm/widgets/inherited.dart';
+
+final topLinkHandler = LinkHandler(
+    test: (input) => RegExp(r"top\/[0-9][0-9]*").hasMatch(input ?? ""),
+    match: (input) =>
+        RegExp(r"(?<=top\/)[0-9][0-9]*").stringMatch(input ?? ""),
+    action: (match, context) async {
+      print('here');
+      await ItemProvider().getItemById(int.parse(match));
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context2) {
+          print('should');
+          return PostPage(index: 0);
+        }),
+      );
+    });
+
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+
   @override
   Widget build(BuildContext context) {
     return MyInherited(
@@ -14,16 +43,12 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primaryColor: Color(0xffee4d2e),
         ),
-        home: HomeView(),
+        home: Builder(
+            builder: (context) => FutureBuilder(
+                  future: AppLinkHandler([topLinkHandler], context).init(),
+                  builder: (context, snap) => HomeView(),
+                )),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-

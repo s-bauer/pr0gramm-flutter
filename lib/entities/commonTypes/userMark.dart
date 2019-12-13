ca8b55cac09996b6787053b2e69c2e54f1d9eb53
Simon Bauer <bauer-s-public@outlook.com>
2019-12-13 16:20:00 +0100

    Refactored UserMark

--- lib/entities/commonTypes/userMark.dart
+++ lib/entities/commonTypes/userMark.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:pr0gramm/entities/commonTypes/baseTypes/enum.dart';
 
+const _userMarkData = [
+  const UserMarkData(name: "Schwuchtel", color: Color(0xffffffff)),
+  const UserMarkData(name: "Neuschwuchtel", color: Color(0xffe108e9)),
+  const UserMarkData(name: "Altschwuchtel", color: Color(0xff5bb91c)),
+  const UserMarkData(name: "Administrator", color: Color(0xffff9900)),
+  const UserMarkData(name: "Gebannt", color: Color(0xff444444)),
+  const UserMarkData(name: "Moderator", color: Color(0xff008fff)),
+  const UserMarkData(name: "Fliesentisch", color: Color(0xff6c432b)),
+  const UserMarkData(name: "LebendeLegende", color: Color(0xff1cb992)), //content:'\25C6';margin-left:0.4em;}
+  const UserMarkData(name: "Wichtel", color: Color(0xffd23c22)), //content:'\25A7';margin-left:0.4em;}
+  const UserMarkData(name: "EdlerSpender", color: Color(0xff1cb992)),
+  const UserMarkData(name: "Mittelaltschwuchtel", color: Color(0xffaddc8d)),
+  const UserMarkData(name: "Altmoderator", color: Color(0xff7fc7ff)),
+  const UserMarkData(name: "Communityhelfer", color: Color(0xffc52b2f)), // content:'\2764';margin-left:0.4em;}
+  const UserMarkData(name: "Nutzerbot", color: Color(0xff10366f)),
+  const UserMarkData(name: "Systembot", color: Color(0xffffc166))
+];
+
+class UserMarkData {
+  final String name;
+  final Color color;
+
+  const UserMarkData({this.name, this.color});
+}
+
 class UserMark extends Enum<int> {
   static UserMark Schwuchtel = UserMark(0);
   static UserMark Neuschwuchtel = UserMark(1);
@@ -18,45 +43,11 @@ class UserMark extends Enum<int> {
   static UserMark Nutzerbot = UserMark(13);
   static UserMark Systembot = UserMark(14);
 
-  UserMark(int val) : super(val);
+  UserMark(int val) : _data = _userMarkData[val], super(val);
 
-  Color get color => _colors[value];
+  final UserMarkData _data;
 
-  String get name => _names[value];
+  Color get color => _data.color;
+  String get name => _data.name;
 }
 
-const _colors = [
-  Color(0xffffffff),
-  Color(0xffe108e9),
-  Color(0xff5bb91c),
-  Color(0xffff9900),
-  Color(0xff444444),
-  Color(0xff008fff),
-  Color(0xff6c432b),
-  Color(0xff1cb992), //content:'\25C6';margin-left:0.4em;}
-  Color(0xffd23c22), //content:'\25A7';margin-left:0.4em;}
-  Color(0xff1cb992),
-  Color(0xffaddc8d),
-  Color(0xff7fc7ff),
-  Color(0xffc52b2f), // content:'\2764';margin-left:0.4em;}
-  Color(0xff10366f),
-  Color(0xffffc166),
-];
-
-const _names = [
-  "Schwuchtel",
-  "Neuschwuchtel",
-  "Altschwuchtel",
-  "Administrator",
-  "Gebannt",
-  "Moderator",
-  "Fliesentisch",
-  "LebendeLegende",
-  "Wichtel",
-  "EdlerSpender",
-  "Mittelaltschwuchtel",
-  "Altmoderator",
-  "Communityhelfer",
-  "Nutzerbot",
-  "Systembot",
-];

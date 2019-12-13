6cba538a016cc226286ce078c184c416542d5630
Simon Bauer <bauer-s-public@outlook.com>
2019-12-13 15:20:15 +0100

    Added | Operator to Flags to support bitwise or

--- lib/entities/commonTypes/flags.dart
+++ lib/entities/commonTypes/flags.dart
@@ -5,7 +5,11 @@ class Flags extends Enum<int> {
   static Flags SFW = Flags(9);
   static Flags NSFW = Flags(2);
   static Flags NSFL = Flags(4);
-  static Flags All = Flags(15);
+  static Flags All = Flags.SFW | Flags.NSFW | Flags.NSFL;
 
   Flags(int val) : super(val);
+
+  Flags operator |(Flags other) {
+    return Flags(value | other.value);
+  }
 }

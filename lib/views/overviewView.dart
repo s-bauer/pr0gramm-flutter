bd5d5c1a5e1c9ebf767487b62cf11a3c3ff818b2
Simon Bauer <bauer-s-public@outlook.com>
2019-12-13 18:29:36 +0100

    Refactored OverviewView

--- lib/views/overviewView.dart
+++ lib/views/overviewView.dart
@@ -32,45 +32,52 @@ class OverviewView extends StatefulWidget {
 
 class _OverviewViewState extends State<OverviewView> {
   final imgProv.ImageProvider _imageProvider = imgProv.ImageProvider();
+  FeedProvider feedProvider;
 
   @override
   Widget build(BuildContext context) {
-    final feedProvider = FeedInherited.of(context).feedProvider;
+    if(feedProvider == null)
+      feedProvider = FeedInherited.of(context).feedProvider;
 
     return GridView.builder(
-      gridDelegate:
-          new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-      itemBuilder: (context, index) {
-        return FutureBuilder<Item>(
-          future: feedProvider.getItem(index),
-          builder: (context, snapshot) {
-            if (snapshot.hasData)
-              return GestureDetector(
-                child: new FutureBuilder(
-                  future: _imageProvider.getThumb(snapshot.data),
-                  builder: (context, snap) {
-                    if (snap.hasData) return Image.memory(snap.data);
+      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+      physics: new AlwaysScrollableScrollPhysics(),
+      itemBuilder: buildItem,
+    );
+  }
 
-                    return Container(color: Colors.red);
-                  },
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => PostPage(
-                        index: index,
-                        feedProvider: feedProvider,
-                      ),
-                    ),
-                  );
-                },
-              );
+  Widget buildItem(BuildContext context, int index) {
+    return FutureBuilder<Item>(
+      future: feedProvider.getItem(index),
+      builder: (context, snapshot) {
+        if (snapshot.hasData)
+          return GestureDetector(
+            child: new FutureBuilder(
+              future: _imageProvider.getThumb(snapshot.data),
+              builder: (context, snap) {
+                return snap.hasData
+                    ? Image.memory(snap.data)
+                    : new Container();
+              },
+            ),
+            onTap: () => onThumbTap(index),
+          );
 
-            return CircularProgressIndicator();
-          },
-        );
+        return Container();
       },
+    );
+
+  }
+
+  void onThumbTap(int index) {
+    Navigator.push(
+      context,
+      new MaterialPageRoute(
+        builder: (context) => PostPage(
+          index: index,
+          feedProvider: feedProvider,
+        ),
+      ),
     );
   }
 }

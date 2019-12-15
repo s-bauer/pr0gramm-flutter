a455bb15223234b5ec97f385f05fbc568d17bc52
Mathis Michel <mathis.michel@outlook.de>
2019-12-15 20:52:42 +0100

    fix eq problem with Enums

--- lib/entities/baseTypes/enum.dart
+++ lib/entities/baseTypes/enum.dart
@@ -2,4 +2,10 @@ abstract class Enum<T> {
   final T value;
 
   Enum(this.value);
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  bool operator ==(other) => value == other.value;
 }

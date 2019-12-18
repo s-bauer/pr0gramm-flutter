57fafd3f5c6eda5097f0733362459213a2ffc103
Mathis Michel <mathis.michel@outlook.de>
2019-12-18 20:03:06 +0100

    build new Vote Button container 'PostVote' with the new VoteButton s using the same VoteAnimationService instance

---
+++ lib/views/post/widgets/post_vote.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:pr0gramm/api/dtos/item/item.dart';
+import 'package:pr0gramm/entities/enums/vote.dart';
+import 'package:pr0gramm/entities/enums/voteButtonType.dart';
+import 'package:pr0gramm/services/vote_animation_service.dart';
+import 'package:pr0gramm/services/vote_service.dart';
+import 'package:pr0gramm/views/post/widgets/vote_button.dart';
+import 'package:pr0gramm/widgets/global_inherited.dart';
+
+class PostVote extends StatelessWidget {
+  final Item item;
+  final Vote initialVote;
+  final VoteService _voteService = VoteService.instance;
+
+  PostVote({
+    Key key,
+    this.item,
+    this.initialVote,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final VoteAnimationService animationService = VoteAnimationService(
+      voteItemHandler: (vote) => _voteService.voteItem(item, vote),
+      initialVote: initialVote,
+    );
+    final isLoggedIn = GlobalInherited.of(context).isLoggedIn;
+    return Row(
+      children: <Widget>[
+        VoteButton(
+          type: VoteButtonType.up,
+          disabled: !isLoggedIn,
+          animationService: animationService,
+        ),
+        VoteButton(
+          type: VoteButtonType.down,
+          disabled: !isLoggedIn,
+          animationService: animationService,
+        ),
+        VoteButton(
+          type: VoteButtonType.favorite,
+          disabled: !isLoggedIn,
+          animationService: animationService,
+        ),
+      ],
+    );
+  }
+}

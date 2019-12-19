592106e120209361a5f246b3fb15acfbecd08571
Simon Bauer <bauer-s-public@outlook.com>
2019-12-19 19:48:20 +0100

    Added retry logic to feed

--- lib/entities/feed.dart
+++ lib/entities/feed.dart
@@ -4,17 +4,21 @@ import 'package:flutter/widgets.dart';
 import 'package:pr0gramm/api/dtos/item/item.dart';
 import 'package:pr0gramm/entities/feed_details.dart';
 import 'package:pr0gramm/services/item_provider.dart';
+import 'package:retry/retry.dart';
 
 import 'enums/feed_type.dart';
 
 class Feed {
   FeedDetails feedDetails;
   final FeedType feedType;
-
   final ItemProvider _itemProvider = new ItemProvider();
+  final retryConfig =
+      new RetryOptions(maxAttempts: 7, maxDelay: Duration(seconds: 5));
 
   List<Item> _forwardList = [];
   List<Item> _backwardsList = [];
+
+  bool _isBusy = false;
 
   final StreamController<List<Item>> _forwardStream =
       new StreamController.broadcast();
@@ -37,31 +41,63 @@ class Feed {
   get backwardData => _backwardsList;
 
   Future loadForward() async {
-    final nextBatch = await _itemProvider.getOlderBatch(feedDetails);
-    if (nextBatch != null) {
-      _forwardList.addAll(nextBatch.items);
-      _forwardStream.add(_forwardList);
+    if(_isBusy)
+      return;
+
+    try {
+      _isBusy = true;
+
+      final nextBatch = await retryConfig
+          .retry(() => _itemProvider.getOlderBatch(feedDetails));
+
+      if (nextBatch != null) {
+        _forwardList.addAll(nextBatch.items);
+        _forwardStream.add(_forwardList);
+      }
+    } finally {
+      _isBusy = false;
     }
   }
 
   Future loadBackwards() async {
-    final prevBatch = await _itemProvider.getNewerBatch(feedDetails);
-    if (prevBatch != null) {
-      _backwardsList.addAll(prevBatch.items);
-      _backwardStream.add(_backwardsList);
+    if(_isBusy)
+      return;
+
+    try {
+      _isBusy = true;
+
+      final prevBatch = await retryConfig
+          .retry(() => _itemProvider.getNewerBatch(feedDetails));
+
+      if (prevBatch != null) {
+        _backwardsList.addAll(prevBatch.items);
+        _backwardStream.add(_backwardsList);
+      }
+    } finally {
+      _isBusy = false;
     }
   }
 
   Future refresh() async {
-    feedDetails = feedDetails.refresh();
+    if(_isBusy)
+      return;
 
-    final firstBatch = await _itemProvider.getFirstBatch(feedDetails);
+    try {
+      _isBusy = true;
 
-    _backwardsList = null;
-    _forwardList = firstBatch.items;
+      feedDetails = feedDetails.refresh();
 
-    _forwardStream.add(_forwardList);
-    _backwardStream.add(_backwardsList);
+      final firstBatch = await retryConfig
+          .retry(() => _itemProvider.getFirstBatch(feedDetails));
+
+      _backwardsList = null;
+      _forwardList = firstBatch.items;
+
+      _forwardStream.add(_forwardList);
+      _backwardStream.add(_backwardsList);
+    } finally {
+      _isBusy = false;
+    }
   }
 
   void dispose() {
